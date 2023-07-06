ed5d3ceb03fde94ccf5cf4a2ff2cff05d8f0f3c6
Bad-Karmaa <caoviethoang.lfay@gmail.com>
2023-07-06 16:50:09 +0200

    env dev to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: true,
+  production: false,
   baseApiUrl: "https://devdolibarrsml.societe-kali.fr/api/index.php",
+  smlPhonenumber: "(+33) 7 53 08 74 88",
+  smlHotline:"(+33) 9 80 80 15 89",
+  smlEmail:"contact@sml-import-export.fr",
+  smlFacebookURL:"https://www.facebook.com/sml.import.export/",
+  smlEuroPageURL:"https://www.europages.fr/SML-IMPORT-EXPORT/00000005437621-001.html",
+  docHashpUrl:"https://devdolibarrsml.societe-kali.fr/document.php?hashp="
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
